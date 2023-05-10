f1977977ed2cf63df8e294300f02dc8645725501
victorcostac <107112671+victorcostac@users.noreply.github.com>
2023-05-10 00:29:28 -0300

    introdução curso node

---
+++ 1_INTRO/3_tarefa_1/prog1.js
@@ -0,0 +1,25 @@
+function soma (a,b){
+    c = a + b
+    return c 
+}
+console.log(soma(10,20))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function soma (a,b){
+//     const c = a + b
+//     return c
+    
+// }
+// console.log(soma(10,20))
