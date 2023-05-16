4f5a1d47314d9bc40db364ea69ec7c99f0f87ad8
victorcostac <107112671+victorcostac@users.noreply.github.com>
2023-05-15 23:44:45 -0300

    4_

--- 2_FUNDAMENTOS/4_ler_argumentos/index.js
+++ 2_FUNDAMENTOS/4_ler_argumentos/index.js
@@ -6,3 +6,11 @@ const args = process.argv.slice(2)
 
 console.log(args)
 
+const nome = args[0].split("=")[1]
+
+const id = args[1].split("=")[1]
+
+console.log(nome, id)
+
+console.log('o nome dele é', nome, 'e ele tem ', id, 'anos!' )
+
